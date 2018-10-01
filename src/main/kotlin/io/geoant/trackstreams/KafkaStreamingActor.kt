b6c8e0238645b30fe4fd7a2c11ce7d74f4243e81
rich delaney <delaney.tech.14@gmail.com>
2018-10-01 10:29:05 -0400

    Modified logic to properly stream and handle multiple input files

--- src/main/kotlin/io/geoant/trackstreams/KafkaStreamingActor.kt
+++ src/main/kotlin/io/geoant/trackstreams/KafkaStreamingActor.kt
@@ -8,16 +8,26 @@ import akka.japi.pf.ReceiveBuilder
 import com.databricks.spark.avro.SchemaConverters
 import com.databricks.spark.xml.XmlReader
 import org.apache.spark.api.java.JavaSparkContext
+import org.apache.spark.sql.Column
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.RowFactory
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.api.java.UDF1
 import org.apache.spark.sql.streaming.Trigger
 import org.apache.spark.sql.types.DataTypes
+import org.apache.spark.sql.types.Metadata
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types.StructType
 import org.apache.spark.streaming.api.java.JavaStreamingContext
 import org.springframework.beans.factory.config.ConfigurableBeanFactory
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
+import java.util.*
+import org.apache.spark.sql.functions.callUDF
+import org.apache.spark.sql.functions.col
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.collections.ArrayList
 import net.simon04.jelementtree.ElementTree as ET
 
 @Component
@@ -32,12 +42,11 @@ class KafkaStreamingActor : AbstractLoggingActor {
     var sqlc: SQLContext
     var sc: JavaSparkContext
     var ssc: JavaStreamingContext
-    var count: Long = 0
+//    var count: Long = 0
 
     val mediator = DistributedPubSub.get(context.system).mediator()
 
     constructor(ssc: JavaStreamingContext, kProps: KafkaProperties, listener: ActorRef) {
-//        constructor(ssc: JavaStreamingContext, kProps : KafkaProperties, elasticWriter : ActorRef, geoWaveWriter : ActorRef, listener: ActorRef) {
 
         var bootstrap = kProps?.bootstrapServers?.joinToString(separator = ",")
         if (bootstrap == null || bootstrap.contentEquals("")) {
@@ -56,23 +65,20 @@ class KafkaStreamingActor : AbstractLoggingActor {
 //                .option("startingOffsets", "latest")
                 .load()
 
-//        val startStopFlag = xmlStream.selectExpr("CAST(value AS STRING)").filter {
-//
-//            it.getString(0).contains("\"mark\":\"START\"") || it.getString(0).contains("\"mark\":\"END\"")
-//        }
-
-//        if ((startStopFlag.select(col("value")).collect()[0] as String).contains("\"mark\":\"START\"")) {
-//            count = 0
-//        }
-
         val values = xmlStream.selectExpr("CAST(value AS STRING)").filter {
-            !(it.getString(0).contains("\"mark\":\"START\"")) && !(it.getString(0).contains("\"mark\":\"END\"")) && !(it.getString(0).contains("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"))
+            !(it.getString(0).contains("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"))
         }
 
-        this.sqlc.udf().register("xmlParse", XmlParser, DataTypes.StringType)
+        val schema = StructType(
+                arrayOf(StructField("id", DataTypes.StringType, false, Metadata.empty()),
+                        StructField("xml", DataTypes.StringType, false, Metadata.empty()))
+        )
+
+        this.sqlc.udf().register("xmlParse", XmlParser, schema)
 
         val queryDf = values
-                .selectExpr("xmlParse(value) as xml")
+                .withColumn("data", callUDF("xmlParse", col("value")))
+                .select(Column("data").getItem("id").`as`("id"), Column("data").getItem("xml").`as`("xml"))
                 .writeStream()
                 .outputMode("append")
                 .format("memory")
@@ -82,106 +88,70 @@ class KafkaStreamingActor : AbstractLoggingActor {
 
         while (queryDf.isActive) {
 
-//            if (cnt.compareTo(0) != 0 && cnt <= this.count) {
-            if (Parser.eof) {
+            Parser.files.forEach {
 
-                log().info("Parser found EOF...")
-                val queryDfTable = this.sqlc.sql("select * from queryDf")
-                queryDf.stop()
-                val cnt = queryDfTable.count()
-                log().info("********************************************************************************* COUNT: $cnt")
+                if (it.value) {
 
+                    log().info("Parser found EOF...")
+                    log().info("Working with id: ${it.key}")
+                    val queryDfTable = this.sqlc.sql("select xml from queryDf where id = '${it.key}'").filter {
+                        !(it.getString(0).contains("\"mark\":\"START\"")) && !(it.getString(0).contains("\"mark\":\"END\""))
+                    }
+                    val cnt = queryDfTable.count()
+                    log().info("********************************************************************************* COUNT: $cnt")
 
-//                val xmlRows: Array<Row> = queryDfTable.collect() as Array<Row>
-                var xmlString = StringBuilder()
-                queryDfTable.toJavaRDD().toLocalIterator().forEach {
-                    xmlString.append(it.getString(0).trim().replaceFirst("^([\\W]+)<", "<"))
+                    var xmlString = StringBuilder()
+                    queryDfTable.toJavaRDD().toLocalIterator().forEach {
+                        xmlString.append(it.getString(0).trim().replaceFirst("^([\\W]+)<", "<"))
+                    }
+
+                    mediator.tell(DistributedPubSubMediator.Send("/user/geowave-writer", xmlString.toString(), false), self)
+
+                    val xmlRdd = this.sc.parallelize(mutableListOf(xmlString.toString()))
+
+                    val xmlDf = XmlReader().withRowTag("TrackMessage").xmlRdd(sqlc, xmlRdd.rdd())
+
+                    var json = StringBuilder()
+                    xmlDf.toJSON().toJavaRDD().toLocalIterator().forEach {
+                        json.append(it)
+                    }
+
+                    mediator.tell(DistributedPubSubMediator.Send("/user/elastic-writer", json.toString(), false), self)
+
+                    log().info("Reset Parser...")
+                    Parser.files.remove(it.key)
+
+                    queryDfTable.show()
                 }
 
-//                val xmlString = (rowsAsStrings as Array<String>).joinToString(separator = "")
-
-//                val xmlString = xmlRows.map { row ->
-//                    row.getString(0).trim().replaceFirst("^([\\W]+)<","<")
-//                }.joinToString(separator = "")
-
-                mediator.tell(DistributedPubSubMediator.Send("/user/geowave-writer", xmlString.toString(), false), self)
-//                geoWaveWriter.tell(xmlString.toString(), self)
-
-                val xmlRdd = this.sc.parallelize(mutableListOf(xmlString.toString()))
-
-                val xmlDf = XmlReader().withRowTag("TrackMessage").xmlRdd(sqlc, xmlRdd.rdd())
-
-//                saveToCassandra(xmlDf)
-//                xmlDf.printSchema()
-//                val json = xmlDf.write().format("json").save()
-//                val jsonArray = xmlDf.toJSON().collect() as Array<String>
-
-                var json = StringBuilder()
-                val jsonArray = xmlDf.toJSON().toJavaRDD().toLocalIterator().forEach {
-                    json.append(it)
-                }
-//                log().info(json)
-
-                mediator.tell(DistributedPubSubMediator.Send("/user/elastic-writer", json.toString(), false), self)
-//                elasticWriter.tell(json, self)
-//                CassandraJavaUtil.javaFunctions(xmlDf.javaRDD()).writerBuilder("lake", "tracks", mapToRow(Row::class.java)).saveToCassandra()
-
-                log().info("Reset Parser...")
-                count = 0
-                Parser.eof = false
-//                queryDf.stop()
-
-                queryDfTable.show()
+            Thread.sleep(10000)
             }
-
-//            Thread.sleep(10000)
         }
-
-//        CassandraStreamingJavaUtil.javaFunctions(
-//                kafkaStream.flatMap {
-//                    mutableListOf(
-//                            XmlEntry(
-//                                    UUID.randomUUID(),
-//                                    it.toString().split("\"text/plain\"")[1])
-//                    ).iterator()
-//                })
-//                .writerBuilder("lake",
-//                        "xml_entries",
-//                        mapToRow(
-//                                XmlEntry::class.java,
-//                                mapOf("id" to "entry_id", "xml" to "entry_data")))
-//                .saveToCassandra()
 
         listener.tell("pipe initialized", self)
     }
 
+    object XmlParser : UDF1<String, Row> {
 
-    object XmlParser : UDF1<String, String> {
-        override fun call(t1: String?): String {
+        var id = UUID.randomUUID()
 
+        override fun call(t1: String?): Row {
             val line = t1!!.split("\"text/plain\"")[1].trim()
-            if (line.contains("</TrackMessage>")) {
-                Parser.eof = true
+            if (line.contains("\"mark\":\"START\"")) {
+                id = UUID.randomUUID()
+                Parser.files.put(id.toString(), false)
             }
-            return line
+            if (line.contains("\"mark\":\"END\"")) {
+                Parser.files.put(id.toString(), true)
+            }
+
+            return RowFactory.create(id.toString(), line)
         }
     }
 
     object Parser {
-        var eof = false
+        var files = ConcurrentHashMap<String, Boolean>()
     }
-
-//    fun saveToCassandra(df : Dataset<Row?>) = {
-//
-//          CassandraJavaUtil.javaFunctions(df.javaRDD()).flatMap {
-//
-//
-//          }
-//              .writerBuilder("lake", "tracks", mapToRow(df.schema().javaClass)).saveToCassandra()
-//
-////        df.write().format("org.apache.spark.sql.cassandra").options(mapOf("keyspace" to "lake", "table" to "tracks"))
-//          }
-//    }
 
 }
 
